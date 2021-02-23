0b40d652f2f7552b713f3fd06c6480989cb4f046
matheusazambuja <matheusazambuja35@gmail.com>
2021-02-22 22:55:32 -0300

    teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import Link from 'next/link'
 
 function Home() {
     return (
-        <h2>Home</h2>
+        <div>
+            <h2>Home</h2>
+
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
+        </div>
     )
 }
 
